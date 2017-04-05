86b8a455c2a0ba6c915a36c8248edb1844a9de01
ChenYutong <b1ncer@foxmail.com>
2017-04-05 22:15:48 +0800

    opacity & size & mountPoint & align

--- modules/RenderNode.js
+++ modules/RenderNode.js
@@ -4,25 +4,29 @@
 import Transform from './lib/Transform'
 import Node from './Node'
 
-export class RenderNode extends Node {
+const DIRECTION = {
+    X: 0,
+    Y: 1,
+    Z: 2
+}
+
+export default class RenderNode extends Node {
 
     constructor(options = {}) {
         super()
         this._cache = {}
         const {
-            transform = Transform.identity
+            transform = Transform.identity,
+            opacity = 1,
+            mountPoint = [0, 0],
+            size = [0, 0],
+            align = [0, 0]
         } = options
         this.transform = transform
-    }
-
-    get finalTransform() {
-        let finalTransform = this.getCache('finalTransform')
-        if (!finalTransform) {
-            let parentFinalTransform = this._parent ? this._parent.finalTransform : Transform.identity
-            finalTransform = Transform.multiply4x4(parentFinalTransform, this.transform)
-            this.setCache('finalTransform', finalTransform)
-        }
-        return finalTransform
+        this.opacity = opacity
+        this.mountPoint = mountPoint
+        this.size = size
+        this.align = align
     }
 
     add(...args) {
@@ -42,6 +46,51 @@ export class RenderNode extends Node {
 
     get transform() {
         return this._transform
+    }
+
+    get calculatedTransform() {
+        const translate = Transform.getTranslate(this.transform)
+        let calX = translate[DIRECTION.X] + this.size[DIRECTION.X] * this.mountPoint[DIRECTION.X],
+            calY = translate[DIRECTION.Y] + this.size[DIRECTION.Y] * this.mountPoint[DIRECTION.Y]
+
+        const parentSize = this._parent ? this._parent.size : [0, 0]
+        calX = parentSize[DIRECTION.X] * this.align[DIRECTION.X] + calX
+        calY = parentSize[DIRECTION.Y] * this.align[DIRECTION.Y] + calY
+
+        const copiedTransform = Object.assign([], this.transform)
+        copiedTransform[12] = calX
+        copiedTransform[13] = calY
+
+        return copiedTransform
+    }
+
+    get finalTransform() {
+        let finalTransform = this.getCache('finalTransform')
+        if (!finalTransform) {
+            let parentFinalTransform = this._parent ? this._parent.finalTransform : Transform.identity
+            finalTransform = Transform.multiply4x4(parentFinalTransform, this.calculatedTransform)
+            this.setCache('finalTransform', finalTransform)
+        }
+        return finalTransform
+    }
+
+    set opacity(value) {
+        this.cleanCache('finalOpacity')
+        this._opacity = value
+    }
+
+    get opacity() {
+        return this._opacity
+    }
+
+    get finalOpacity() {
+        let finalOpacity = this.getCache('finalOpacity')
+        if (!finalOpacity) {
+            let parentFinalOpacity = this._parent ? this._parent.finalOpacity : 1
+            finalOpacity = parentFinalOpacity * this.opacity
+            this.setCache('finalOpacity', finalOpacity)
+        }
+        return finalOpacity
     }
 
     getCache(prop) {
--- modules/__tests__/render-node-test.js
+++ modules/__tests__/render-node-test.js
@@ -21,25 +21,39 @@ describe('RenderNode', () => {
         done()
     })
 
+    const renderNode2 = new RenderNode({
+        transform: Transform.translate(100, 100, 100)
+    })
+
+    const renderNode3 = new RenderNode({
+        transform: Transform.translate(400, 400)
+    })
+
+    const renderNode4 = new RenderNode()
+
     it('add renderNode with transform', done => {
-        const renderNode2 = new RenderNode({
-            transform: Transform.translate(100, 100, 100)
-        })
 
         renderNode.add(renderNode2)
         expect(Transform.getTranslate(renderNode2.finalTransform)).toEqual([100, 100, 100])
 
-        const renderNode3 = new RenderNode({
-            transform: Transform.translate(400, 400)
-        })
         expect(Transform.getTranslate(renderNode3.finalTransform)).toEqual([400, 400, 0])
 
         renderNode2.add(renderNode3)
         expect(Transform.getTranslate(renderNode3.finalTransform)).toEqual([500, 500, 100])
 
-        const renderNode4 = new RenderNode()
         renderNode2.add(renderNode4)
-        const final4 = renderNode4.finalTransform
+
+        done()
+    })
+
+    it('verify cache auto clean', done => {
+        renderNode2.transform = Transform.translate(200, 100, 0)
+
+        expect(Transform.getTranslate(renderNode2.finalTransform)).toEqual([200, 100, 0])
+
+        expect(Transform.getTranslate(renderNode3.finalTransform)).toEqual([600, 500, 0])
+
+        expect(Transform.getTranslate(renderNode4.finalTransform)).toEqual([200, 100, 0])
 
         done()
     })
