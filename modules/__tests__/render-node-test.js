788a9bc5e28ae674dd808e7d4f95788c41c71523
b1ncer <b1ncer@foxmail.com>
2017-04-06 04:35:45 +0800

    Fix: add & remove & cleanCache Feature: eventListener & output

--- modules/__tests__/render-node-test.js
+++ modules/__tests__/render-node-test.js
@@ -5,6 +5,12 @@ import RenderNode from '../RenderNode'
 import expect from 'expect'
 import Transform from '../lib/Transform'
 
+//代码修改后第一次执行测试，requestAnimationFrame 执行不了，之后刷新浏览器就没问题
+//暂时 hack 了
+window.requestAnimationFrame = function(fn) {
+    setTimeout(fn, 0)
+}
+
 describe('RenderNode', () => {
 
     const renderNode = new RenderNode()
@@ -54,6 +60,25 @@ describe('RenderNode', () => {
         expect(Transform.getTranslate(renderNode3.finalTransform)).toEqual([600, 500, 0])
 
         expect(Transform.getTranslate(renderNode4.finalTransform)).toEqual([200, 100, 0])
+
+        done()
+    })
+
+    it('listen to output once', done => {
+        let addListenerSuccess = false
+        addListenerSuccess = renderNode3.once(({finalTransform}) => {
+
+            expect(finalTransform).toBe(renderNode3.finalTransform)
+            done()
+        })
+
+        expect(typeof addListenerSuccess).toBe('function')
+        renderNode2.transform = Transform.translate(500, 100, 10)
+    })
+
+    it('listener added by once func will automatic delete', done => {
+
+        expect(renderNode3._listener.size).toBe(0)
 
         done()
     })
